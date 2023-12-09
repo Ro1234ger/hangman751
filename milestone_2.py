804007e6b79e6bc34979534d2af8262aedcb18c8
Roger <150137693+Ro1234ger@users.noreply.github.com>
2023-12-09 15:29:50 +0000

    It is basic codes of a hangman game.
    There is a list of words in code, and a Random.choice is used to generate a word.

---
+++ milestone_2.py
@@ -0,0 +1,7 @@
+import random
+
+fruit_name = {"apple","peach","strawberry","watermelon","grape"}
+word_list = list(fruit_name)
+word = random.choice(word_list)
+
+guess = input ("Please input a letter:")
