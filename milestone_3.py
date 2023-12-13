97628836abe5fbe2c49e89c0ffe67d4ef9a6a98d
Roger <rzj20010824@outlook.com>
2023-12-13 17:56:12 +0000

    Change the main code of Ask-for-input and Check-guess to function

--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,30 @@
-from milestone_2 import generate_word
+from milestone_2 import generate_word  # Assuming generate_word is defined in the milestone_2 module
 
-while True:
-    guess = input("Please input a letter:")
+def check_guess(guess):
+    """
+    Check if the guessed letter is in the word.
+    """
+    guess = guess.lower()  # Step 2: Convert the guess to lowercase
 
-    if guess.isalpha() and len(guess) == 1:
-        break
+    if guess in generate_word:  # Step 3: Check if the guess is in the word
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess in generate_word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input():
+    """
+    Ask the user for a valid input.
+    """
+    while True:
+        guess = input("Please input a letter:")
+
+        if guess.isalpha() and len(guess) == 1:  # Step 2: Check if the input is a valid letter
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+    check_guess(guess)  # Step 3: Call the check_guess function to check if the guess is in the word
+
+# Step 4: Call the ask_for_input function to test the code
+ask_for_input()
+
